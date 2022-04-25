b2c67cb4f8f5640311dc495b63665146b98a9ac1
PizzaYeti <lexi@Pizzayetimakes.com>
2022-04-25 11:47:12 -0500

    All the weekly challenges files set up

---
+++ Week4_Challenge.js
@@ -0,0 +1,18 @@
+// Longest Word
+// Have the function LongestWord(sen) take the sen parameter being passed and return the longest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty. Words may also contain numbers, for example "Hello world123 567"
+// Examples
+// Input: "fun&!! time"
+// Output: time
+// Input: "I love dogs"
+// Output: love
+
+//Starter Code
+function LongestWord(sen) { 
+
+    // code goes here  
+    return sen; 
+  
+  }
+     
+  // keep this function call here 
+  console.log(LongestWord(readline()));
